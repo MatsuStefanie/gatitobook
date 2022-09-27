3e24ce54fc52711903828cd38e639a196d6bf316
Stefanie Silva <matsustefanie@gmail.com>
2022-09-27 19:15:15 -0300

    Projeto de boas praticas em angular

---
+++ src/app/home/login/login.component.ts
@@ -0,0 +1,26 @@
+import { AutenticacaoService } from './../../autenticacao/autenticacao.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  usuario = '';
+  senha = '';
+  constructor(private authService: AutenticacaoService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+  login() {
+    this.authService.autentica(this.usuario, this.senha).subscribe(() => {
+      this.router.navigateByUrl('animais');
+    }, (error) => {
+      alert('Usuário ou senha inválido');
+      console.error(error);
+    })
+  }
+}
